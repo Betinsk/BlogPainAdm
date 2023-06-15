3cd0b3b65d52babee4aba23fb2ae733c224d091a
Betinsk <gmdust@htomail.com>
2023-06-15 12:21:34 -0300

    a

--- article/ArticleController.js
+++ article/ArticleController.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('../category/Category')
+const Article = require('')
 
-router.get('/articles', (req, res) => {
-    res.send('ROTA DE ARTIGOS')
+router.get('/articles/new', (req, res) => {
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', {
+            categories: categories
+        })
+
+    })
 })
+
+
 
 module.exports = router
